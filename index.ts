9b4706015f6bf757b6a3638a4d146594bdb09277
ad <39013925+ad2017gd@users.noreply.github.com>
2026-07-17 08:42:14 +0300

    a

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { Hook } from 'require-in-the-middle';
 import {execSync} from 'child_process'
 import fs from 'fs';
 import 'temporal-polyfill/global'
+import {Semaphore} from 'await-semaphore';
 
 // stupid fucking fix
 new Hook(['gm'], { internals: true }, function (exports, name, basedir) {
@@ -11,7 +12,7 @@ new Hook(['gm'], { internals: true }, function (exports, name, basedir) {
 })
 const i2a = require('image-to-ascii')
 
-let config : {file?:string, font:string, fontsize:number, size: number[], fps:number, offset: number} = { size: [50,15], fps: 4, fontsize: 12, font:"Arial", offset: 0};
+let config : {file?:string, font:string, fontsize:number, size: number[], fps:number, offset: number, from?: string, to?: string} = { size: [50,15], fps: 4, fontsize: 12, font:"Arial", offset: 0};
 
 let queue = process.argv.slice(2);
 let showHelp = false;
@@ -50,6 +51,16 @@ while(queue.length) {
             queue = queue.slice(1);
             break;
         }
+        case "--from": {
+            config.from = val;
+            queue = queue.slice(1);
+            break;
+        }
+        case "--to": {
+            config.to = val;
+            queue = queue.slice(1);
+            break;
+        }
         case "-?":
         case "-h":
         case "/?":
@@ -65,15 +76,17 @@ while(queue.length) {
 if(!config.file || showHelp) {
     if(!showHelp) console.log("No file specified. Use --file \"filename\" to specify target video.\n");
     console.log(`Arguments:`);
-    console.log(`--file "filename"        -  set target video file`)
-    console.log(`--fontsize [number]      -  set font size`)
+    console.log(`--file "filename"         -  set target video file`)
+    console.log(`--fontsize [number]       -  set font size`)
 
     // commented since they do not actually render on youtube for some reason
 
-    //console.log(`--font "name"            -  set font by name`)
-    console.log(`--fps [number]           -  set target subtitle fps`)
-    //console.log(`--size [number]x[number] -  set target subtitle dimensions\n`)
-    console.log(`--offset [number]        -  set subtitle offset in milliseconds\n`)
+    //console.log(`--font "name"             -  set font by name`)
+    console.log(`--fps [number]            -  set target subtitle fps`)
+    //console.log(`--size [number]x[number]  -  set target subtitle dimensions\n`)
+    console.log(`--offset [number]         -  set subtitle offset in milliseconds\n`)
+    console.log(`--from [number/timestamp] -  set video start seconds/timestamp\n`)
+    console.log(`--to [number/timestamp]   -  set video end seconds/timestamp\n`)
     process.exit(showHelp ? 0: 1)
 }
 
@@ -84,41 +97,68 @@ fs.readdirSync("frames").forEach((s,i)=>{
   fs.unlinkSync("frames/"+s)
 })
 console.log("Extracting frames.")
-execSync(`ffmpeg -i ${config.file} -r ${config.fps} "frames/frame_%04d.jpg" 2> nul`)
 
-console.log("Converting frames.")
+try {
+execSync(`ffmpeg -i ${config.file} ${config.from ? `-ss ${config.from}` : ""} ${config.to ? `-to ${config.to}`: ""} -r ${config.fps} "frames/frame_%04d.jpg" 2> nul`)
+} catch {
+    console.error("Error trying to run ffmpeg. Is it installed?")
+    process.exit(1);
+}
 
-let converted_frames = [];
-let time = Temporal.Duration.from({milliseconds: config.offset}).round({largestUnit:"minutes"});
-for(let s of fs.readdirSync("frames")) {
-    let resolve = (val: any) => {};
-    let promise = new Promise((res,rej) => {resolve=res});
+let files = fs.readdirSync("frames");
+process.stdout.write(`Converting frames. (0/${files.length})`);
+
+let converted_frames = {};
+
+let idx = 1;
+
+
+let semaphore = new Semaphore(10);
+let promises = []
+for(let s in files) {
+
+    let release = await semaphore.acquire();
+    let finish = (a:any) => {};
+
+    promises.push(new Promise((res, rej) => finish = res));
+    
     //<time begin="00:00.0"/><clear/>
-    i2a("frames/"+s, {colored: true, stringify:false, concat: false, size: {width: config.size[0], height: config.size[1]}, pixels: "█"}, (e, c) => {
+    i2a("frames/"+files[s], {colored: true, stringify:false, concat: false, size: {width: config.size[0], height: config.size[1]}, pixels: "█"}, (e, c) => {
 
                                                         // maybe i did pick the goofiest library to use but oh well
         let format = c.map(x=>x.map(y=>`<font color="#${y.pixel.r.toString(16).padStart(2, '0')}${y.pixel.g.toString(16).padStart(2, '0')}${y.pixel.b.toString(16).padStart(2, '0')}">${y.char}</font>`).join(""))
         let line = format.join("<br/>")
         //console.log(format)
-
-        let oldtime = time.add({milliseconds:0}).round({largestUnit:"minutes"});
-
-        time = time.add(Temporal.Duration.from({milliseconds: 1000/(config.fps)}));
-        time = time.round({largestUnit:"minutes"})
+        //console.log(config.offset*1000+(1000000/config.fps)*Number(s), config.fps, config.offset, Number(s))
+        let oldtime = Temporal.Duration.from({microseconds: config.offset*1000+(1000000/config.fps)*Number(s)}).round({largestUnit:"minutes"})
+        let time = oldtime.add({microseconds: (1000000/config.fps)}).round({largestUnit:"minutes"})
 
         let frame = `<time begin="${oldtime.minutes.toString().padStart(2,'0')}:${oldtime.seconds.toString().padStart(2,'0')}.${oldtime.milliseconds.toString().padStart(3,'0')}" end="${time.minutes.toString().padStart(2,'0')}:${time.seconds.toString().padStart(2,'0')}.${time.milliseconds.toString().padStart(3,'0')}"/><clear/>${line}`;
-        converted_frames.push(frame);
+        converted_frames[s] = (frame);
+        //console.log(s, time.seconds, time.milliseconds)
 
         //console.log(JSON.stringify(c))
-        resolve(1);
+        release();
+        finish(1);
+        process.stdout.write(`\rConverting frames. (${idx++}/${files.length})`);
     })
-    await promise;
+
+    
 }
-console.log(converted_frames)
+await Promise.all(promises);
+
+// now we must construct array in order!
+
+let ordered = [];
+for(let i = 0; i < Object.values(converted_frames).length; i++) {
+    ordered[i] = converted_frames[i];
+}
+
 fs.writeFileSync("output.rt", 
 `<window>
 <font size="${config.fontsize}" face="${config.font}">
-${converted_frames.join("\n")}
+${Object.values(ordered).join("\n")}
 </font>
 </window>`
 )
+console.log("\nDone.")
